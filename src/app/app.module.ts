867e5461409732788d47951bb232ad1cd555b69a
perry <User@Tipzee-Aspire.(none)>
2020-06-28 12:49:06 +0800

    Loading indicator interceptor implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { HomeModule } from './modules/home/home.module';
 import { CoreModule } from '@core/core.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ToggleLoadingIndicatorInterceptor } from '@core/libs/loading-indicator/interceptors/toggle-loading-indicator.interceptor';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,13 @@ import { CoreModule } from '@core/core.module';
     AuthModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ToggleLoadingIndicatorInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/modules/auth/pages/login-page/login-page.component.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.ts
@@ -5,7 +5,6 @@ import { Router } from '@angular/router';
 import { ILogin } from '../../interfaces/login.interface';
 import { NotificationService } from '@core/services/notification.service';
 import { ButtonLoaderService } from '@core/libs/button-loader/services/button-loader.service';
-import { LoadingIndicatorService } from '@core/libs/loading-indicator/services/loading-indicator.service';
 
 @Component({
   selector: 'app-login-page',
@@ -17,23 +16,11 @@ export class LoginPageComponent implements OnInit {
   constructor(
     private _authService: AuthService,
     private _btnLoaderService: ButtonLoaderService,
-    private _loadingIndicatorService: LoadingIndicatorService,
     private _notificationService: NotificationService,
     private _router: Router
   ) { }
 
   ngOnInit(): void {}
-
-  showFeedback() {
-    //
-    this._btnLoaderService.show();
-    this._loadingIndicatorService.show();
-  }
-
-  hideFeedback() {
-    this._btnLoaderService.hide();
-    this._loadingIndicatorService.hide();
-  }
 
   doLogin(formData: IFormSubmit<ILogin>) {
     //
@@ -43,7 +30,7 @@ export class LoginPageComponent implements OnInit {
       return;
     }
 
-    this.showFeedback();
+    this._btnLoaderService.show();
 
     this._authService
       .login(formData.values)
@@ -52,11 +39,11 @@ export class LoginPageComponent implements OnInit {
         if (response.success) {
           this._router.navigate(['/home'])
         }
-        this.hideFeedback();
+        this._btnLoaderService.hide();
       }, fail => {
         //
         this._notificationService.notifyError(fail);
-        this.hideFeedback();
+        this._btnLoaderService.hide();
       })
   }
 
